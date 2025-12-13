8e4a289b2ebba6e6f51bc18ea462af1bf0736ea0
Pedro Reis <pedrorochareis256@gmail.com>
2025-12-13 00:45:04 +0000

    Remoção de item do carrinho.php

---
+++ Remoção de item do carrinho.php
@@ -0,0 +1,13 @@
+<?php
+$carrinho = ["Arroz", "Feijão", "Macarrão"];
+
+$itemRemover = "Feijão";
+
+if (($key = array_search($itemRemover, $carrinho)) !== false) {
+    unset($carrinho[$key]);
+}
+
+foreach ($carrinho as $item) {
+    echo $item . "<br>";
+}
+?>
